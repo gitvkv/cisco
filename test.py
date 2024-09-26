2d97d3915cb019b24d24dabc5d343e698cc50f01
gitvkv <67689791+gitvkv@users.noreply.github.com>
2024-09-26 13:54:28 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("Hello:Test")
+devices:
+  - hostname: 192.168.1.1
+    device_type: arista_eos
+  - hostname: 192.168.1.2
+    device_type: arista_eos
+  - hostname: 192.168.1.3
+    device_type: arista_eos
